78b8ddbed7bc526f863a51d31450703fa650d5f0
Alex-C96 <alexander15clarke@gmail.com>
2025-05-18 00:58:27 -0500

    Initial commit with Model Issue and DbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using IssueTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IssueTracker.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Issue> Issues { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("connect string");
+        }
+    }
+}
---
+++ Models/Issue.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace IssueTracker.Models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public IssueStatus Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public enum IssueStatus
+    {
+        Open,
+        InProgress,
+        Resolved,
+        Closed
+    }
+
+}
